06b8bfe067f566fad4b21acabf6176d5f7fd1279
PedroItaloBC <pedroitalobc@gmail.com>
2022-12-16 18:39:54 -0300

    feat: Add core components

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,43 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextPorps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    size: 'md',
+    children: 'Exemple text',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+
+      control: {
+        type: 'inline-radio',
+      },
+    },
+  },
+} as Meta<TextPorps>
+
+export const Primary: StoryObj<TextPorps> = {}
+
+export const CustomTag: StoryObj<TextPorps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
